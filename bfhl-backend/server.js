297d23b7c2fbc2c73c01097b659004d800e666df
aakash1205 <114174835+aakash1205@users.noreply.github.com>
2024-09-22 19:03:15 +0530

    push

--- bfhl-backend/server.js
+++ bfhl-backend/server.js
@@ -72,7 +72,7 @@ app.get('/bfhl', (req, res) => {
 });
 
 // Start the server
-const port = process.env.PORT ||4000;
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });

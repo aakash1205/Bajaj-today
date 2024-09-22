68e8a53312347172c0096ec5f4911301b0e8838f
Aakash <114174835+aakash1205@users.noreply.github.com>
2024-09-22 19:29:26 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+   server: {
+    host: true,  // Listen on all IP addresses (0.0.0.0)
+    port: process.env.PORT || 3000,  // Use the PORT from Render, or fallback to 3000
+    strictPort: true  // Fail if the port is already in use
+  }
 })
